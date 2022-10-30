51c45e3b650b8db2dba52292227304a16b983bda
Fatma Bouzouita <102266091+fatma-bouzouita@users.noreply.github.com>
2022-10-30 15:22:19 +0100

    Add files via upload

---
+++ geometry/codeforces_1199B.cpp
@@ -0,0 +1,20 @@
+    #include <bits/stdc++.h>
+    #define FAST ios::sync_with_stdio(0), cin.tie(0),cout.tie(0)
+    #define MP make_pair
+    #define PB push_back
+    #define ll long long
+    #define FOR(x,i,n) for(int (i)=(x);(i)<(n);(i)++)
+    using namespace std;
+ int main()
+    {//freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+
+float h,l;
+cin>>h>>l;
+cout<< setprecision(10)<<(l*l-h*h)/(2*h);
+
+//pythagore
+//x+h)2=l2+x2
+//2xh=l^2-h^2
+
+}
